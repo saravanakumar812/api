76deec041255a93a252eebd9afb028fb5c0ebf70
saravanaKumar8122 <saravanakumar.mdq@gmail.com>
2024-10-28 16:04:50 +0530

    super

--- routes/food.js
+++ routes/food.js
@@ -5,15 +5,26 @@ const router = express.Router();
 
 // Configure multer to store images in an 'uploads' directory
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: function (req, file, cb) {
     cb(null, "../uploads");
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-    console.log(file.originalname);
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + uniqueSuffix);
   }
 });
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter
+});
 
 // Route to handle product creation with an image
 router.post("/", upload.single("image"), async (req, res) => {
@@ -26,7 +37,7 @@ router.post("/", upload.single("image"), async (req, res) => {
     });
 
     await food.save();
-    res.status(201).json({ message: "Product created successfully", product });
+    res.status(200).json({ message: "Product created successfully", food });
   } catch (error) {
     res.status(500).json({ message: "Error creating product", error });
   }
